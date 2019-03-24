dc9f716a3d91212e49b38f8bc46a29d37334b753
NoScript <weerawat.wic@msu.ac.th>
2019-03-24 16:47:42 +0700

    Edit UserInput

--- TvsB/Script/__Input/UserInput.cs
+++ TvsB/Script/__Input/UserInput.cs
@@ -1,43 +1,57 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using UnityEngine;
 
 namespace TaengvsBug.Script
 {
-    public class UserInput : IUser
+    public class UserInput : MonoBehaviour
     {
-        Dictionary<ConsoleKey, Action> ExecuteMethod = new Dictionary<ConsoleKey, Action>();
+        Dictionary<string, Action> ExecuteMethod = new Dictionary<string, Action>();
 
-        public UserInput()
-        {
-            ExecuteMethod[ConsoleKey.A] = () => this.Attack();
-            ExecuteMethod[ConsoleKey.S] = () => this.Def();
-            ExecuteMethod[ConsoleKey.D] = () => this.Heal();
+        public void Awake()
+        { 
+            ExecuteMethod["A"] = () => this.Attack();
+            ExecuteMethod["S"] = () => this.Def();
+            ExecuteMethod["D"] = () => this.Heal();
 
-            Console.WriteLine("Press: {0}--> ATK, {1}--> DEF, {2}--> HEAL","A","S","D");
-
-            InputKey();
+            Debug.Log("Press: {0}--> ATK, {1}--> DEF, {2}--> HEAL"+ "A"+ "S"+ "D");
         }
 
-        public void InputKey()
+        public void Update()
         {
-            do
+            if (Input.anyKey)
             {
-                ExecuteMethod[Execute()]?.Invoke();
+                if (!ExecuteMethod.ContainsKey(Execute()))
+                {
+                    Debug.Log("Don't Have" + Input.inputString);
+                    return;
+                }
+                else ExecuteMethod[Execute()]?.Invoke();
             }
-            while (true);
+
         }
 
-        void Attack() { Controller.Instance.Attack(); }
-        void Def() { Controller.Instance.HideBug(); }
-        void Heal() { Controller.Instance.PretendToDie(); }
+        void Attack()
+        {
+            Controller.instance.Attack();
+            Debug.Log("Attack");
+        }
+        void Def()
+        {
+            Controller.instance.HideBug();
+            Debug.Log("Def");
+        }
+        void Heal()
+        {
+            Controller.instance.PretendToDie();
+            Debug.Log("Heal");
+        }
 
         //----- 
-        public ConsoleKey Execute()
+        public string Execute()
         {
-           Console.TreatControlCAsInput = true;
-           ConsoleKeyInfo getKeyFromUser = Console.ReadKey();
-           return getKeyFromUser.Key;
+            string getkey = Input.inputString;
+            return getkey.ToUpper();
         }
 
     }//Class
